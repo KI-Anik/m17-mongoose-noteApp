b2e170ca813c61ca0384af1bce123b9ae9c15822
KI-Anik <iamkhairul101@gmail.com>
2025-06-15 17:19:04 +0600

    feat: enhance note schema and update routes for note creation and retrieval

--- src/app.ts
+++ src/app.ts
@@ -3,25 +3,59 @@ import { model, Schema } from 'mongoose'
 
 
 const app: Application = express()
+app.use(express.json())
 
 const noteSchema = new Schema({
-    name: String,
-    content: String
+    title: { type: String, required: true, trim: true },
+    content: { type: String, default: '' },
+    category: {
+        type: String,
+        category: ["personal", "work", "study"],
+        default: 'personal'
+    },
+    pinned: { type: Boolean, default: false },
+    tags: {
+        label: { type: String, required: true },
+        color: { type: String, default: 'green' }
+    }
 })
 
 const Note = model('Note', noteSchema)
 
-app.post('/create', async (req: Request, res: Response) => {
-    const myNote = new Note({
-        name: 'Express',
-        content: 'i am learning Express'
-    })
-    await myNote.save()
+app.post('/notes/create', async (req: Request, res: Response) => {
+
+    // approach 1
+    // const myNote = new Note({
+    //     title: 'TypeScript',
+    //     tags: {
+    //         label: 'backend'
+    //     }
+    // })
+    // await myNote.save()
+
+    // approach 2
+    const body = req.body
+    const note = await Note.create(body)
 
     res.status(201).json({
         success: true,
-        note: myNote
+        // note: myNote
+        NewNote: note
     })
+})
+
+app.get('/notes', async (req: Request, res: Response) => {
+    const result = await Note.find()
+
+    res.status(201).send(result)
+})
+
+app.get('/notes/:noteId', async (req: Request, res: Response) => {
+    const noteId = req.params.noteId
+    const result = await Note.findById(noteId)
+    // const result = await Note.findOne({title : noteId})
+
+    res.status(201).send(result)
 })
 
 app.get('/', (req: Request, res: Response) => {
